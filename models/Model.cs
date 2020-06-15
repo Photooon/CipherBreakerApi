3395ec7453ad4a73b67fedfcc900975e51767b70
Photooon <1527842029@qq.com>
2020-06-15 15:07:16 +0800

    更改主键

--- models/Model.cs
+++ models/Model.cs
@@ -6,50 +6,50 @@ namespace CipherBreakerApi.Models
     public class EncryptItem
     {
         [Key,Column(Order = 0)]
+        public string datetime{ get; set; }
+
+        //[Key,Column(Order = 0)]
         public string method{ get; set; }
 
-        [Key,Column(Order = 1)]
+        //[Key,Column(Order = 1)]
         public string str{ get; set; }
 
-        [Key,Column(Order = 2)]
+        //[Key,Column(Order = 2)]
         public string key{ get; set; }
 
         public string result{ get; set; }
-
-        [Key,Column(Order = 3)]
-        public string datetime{ get; set; }
     }
 
     public class DecryptItem
     {
         [Key,Column(Order = 0)]
+        public string datetime{ get; set; }
+
+        //[Key,Column(Order = 0)]
         public string method{ get; set; }
 
-        [Key,Column(Order = 1)]
+        //[Key,Column(Order = 1)]
         public string str{ get; set; }
 
-        [Key,Column(Order = 2)]
+        //[Key,Column(Order = 2)]
         public string key{ get; set; }
 
         public string result{ get; set; }
-
-        [Key,Column(Order = 3)]
-        public string datetime{ get; set; }
     }
 
     public class BreakItem
     {
         [Key,Column(Order = 0)]
+        public string datetime{ get; set; }
+
+        //[Key,Column(Order = 0)]
         public string method{ get; set; }
 
-        [Key,Column(Order = 1)]
+        //[Key,Column(Order = 1)]
         public string str{ get; set; }
 
         public string result_str{ get; set; }
 
         public double result_prob{ get; set; }
-
-        [Key,Column(Order = 2)]
-        public string datetime{ get; set; }
     }
 }
